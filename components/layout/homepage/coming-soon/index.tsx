83870849019710ed27aeba406a006a1c1ca4f009
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-08-03 12:57:45 +0530

    new design with responsive breakpoints

--- components/layout/homepage/coming-soon/index.tsx
+++ components/layout/homepage/coming-soon/index.tsx
@@ -1,3 +1,4 @@
+import PrimaryButton from "@components/buttons/primaryButton";
 import PrimaryHeading from "@components/heading/primaryHeading";
 import SecondaryHeading from "@components/heading/seondaryHeading";
 import ImageComponent from "@components/imageComponent";
@@ -7,39 +8,95 @@ import {
   ContactGroup,
   SinhalaText,
   ImageWrapper,
+  CanvasBG,
+  BlobOne,
+  BlobTwo,
+  BlobThree,
+  BackgroundOverlay,
+  CanvasFG,
+  Logo,
+  CTABox,
+  CTAItem,
+  CTAIcon,
 } from "./styles";
 
 const ComingSoon: React.FC = (): JSX.Element => {
   return (
     <Canvas>
-      <TextGroup>
-        <PrimaryHeading>New website is under development</PrimaryHeading>
-        <SecondaryHeading sinhala>
-          නව වෙබ් අඩවිය සංවර්ධනය වෙමින් පවතී
-        </SecondaryHeading>
-        <ContactGroup>
-          <SinhalaText>
-            පරිපාලන සේවා, ක්‍රම සම්පාදන සේවා, අධ්‍යාපන පරිපාලන සේවා, ගණකාධිකරණ
-            සේවා, විද්‍යාත්මක සේවා විවෘත තරග විභාගයන් සදහා වු පොදු ප්‍රවේශ
-            විභාගය සදහා නව පන්ති ආරම්භය
-          </SinhalaText>
-        </ContactGroup>
-        <ContactGroup>
-          <SinhalaText>වැඩිදුර තොරතුරැ සඳහා අමතන්න.</SinhalaText>
-          <SecondaryHeading sinhala>ඕෂධ සර් - 071 6 720 720</SecondaryHeading>
-        </ContactGroup>
-      </TextGroup>
-      <ImageWrapper>
-        <ImageComponent
-          image={{
-            src: "/assets/img/coming-soon.png",
-            alt: "CATEXAMS.LK | Sri Lanka's leading common admission test tutor ",
-            blurUrl: "data:image/svg+xml;base64,LCHeX+KQ3=+ruhnMvnS*TcV@n3oz",
-          }}
-          sizes={{ width: "100%", height: "80%" }}
-          pos={{ x: "bottom", y: "center" }}
-        />
-      </ImageWrapper>
+      <CanvasBG>
+        <BackgroundOverlay />
+        <BlobOne viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
+          <path d="M416.5,306Q362,362,306,425Q250,488,175.5,443.5Q101,399,65,324.5Q29,250,58.5,169Q88,88,169,53Q250,18,327,57Q404,96,437.5,173Q471,250,416.5,306Z"></path>
+        </BlobOne>
+        <BlobTwo viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
+          <path d="M435.5,303.5Q416,357,384,415Q352,473,287.5,456Q223,439,161.5,431.5Q100,424,74.5,366.5Q49,309,46.5,249.5Q44,190,71,132.5Q98,75,158.5,55Q219,35,281.5,39Q344,43,384,90.5Q424,138,439.5,194Q455,250,435.5,303.5Z"></path>
+        </BlobTwo>
+        <BlobThree viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
+          <path d="M411,313Q376,376,313,432.5Q250,489,176,443.5Q102,398,64.5,324Q27,250,69.5,181Q112,112,181,61Q250,10,330.5,49.5Q411,89,428.5,169.5Q446,250,411,313Z"></path>
+        </BlobThree>
+      </CanvasBG>
+      <CanvasFG>
+        <Logo src="/assets/logo/logo-dark.svg"></Logo>
+        <TextGroup>
+          <PrimaryHeading sinhala alignment="left">
+            පොදු ප්‍රවේශ විභාගය ඇතුළු රජයේ සියලුම තරග විභාග සදහා නිවැරිදිම සහ
+            විශ්වාසනීයම මඟ පෙන්වීම.
+          </PrimaryHeading>
+          <SecondaryHeading sinhala alignment="left">
+            ඕෂධ අල්විස්
+          </SecondaryHeading>
+        </TextGroup>
+        <ImageWrapper>
+          <ImageComponent
+            image={{
+              src: "/assets/img/group.jpg",
+              alt: "CATEXAMS.LK | Sri Lanka's leading common admission test tutor ",
+              blurUrl: "data:image/svg+xml;base64,LCHeX+KQ3=+ruhnMvnS*TcV@n3oz",
+            }}
+          />
+        </ImageWrapper>
+        <CTABox>
+          <CTAItem>
+            <CTAIcon>
+              <use xlinkHref="/assets/svg/sprites.svg#telegram" />
+            </CTAIcon>
+            <SecondaryHeading>Join our Telegram Group</SecondaryHeading>
+            <PrimaryButton
+              isTargetBlank={true}
+              link="https://l.facebook.com/l.php?u=https%3A%2F%2Ft.me%2Fjoinchat%2FnYtuGvC9W8FjODQ1%3Ffbclid%3DIwAR3dgoQLJuJOEsm5VDX5xlLaGvQL5d3WmjcXWGvJx3ZWKm_HUauZrPzDRGc&h=AT277P_icpExRFiedE2j1QPgAnSxIfCSwZcJ6j4vRcTNiw9ymES5K7rMkGMZIwrGEUrJr3L6ShFtvhMc06UEaK-eHlldRXPFmbjLJ06LlbxQr2lj49W7oy0YlXFO&__tn__=-UK-R&c[0]=AT190bW4TxSKQEwtonId1_5QWGIYaxc9tLf3QGHtAmJc-yz8ZOq-Fqiq0hAGtEjRfAgS1WQQwnDHnTmdTvyiyFyOvj0xAcOd0hpR4RH3mGebNDDZBgFOSYXGxgp1EAVQoClfU69b7ugLOwqyvZhPUkfCa-1_3XgJQYqyBuXBP7K0fA"
+              type="link"
+            >
+              Join
+            </PrimaryButton>
+          </CTAItem>
+          <CTAItem>
+            <CTAIcon>
+              <use xlinkHref="/assets/svg/sprites.svg#whatsapp" />
+            </CTAIcon>
+            <SecondaryHeading>Contact us on whatsApp</SecondaryHeading>
+            <PrimaryButton
+              isTargetBlank={true}
+              link="https://wa.me/qr/OJJS7PFEWZZUA1"
+              type="link"
+            >
+              Contact us
+            </PrimaryButton>
+          </CTAItem>
+          <CTAItem>
+            <CTAIcon>
+              <use xlinkHref="/assets/svg/sprites.svg#facebook" />
+            </CTAIcon>
+            <SecondaryHeading>Follow our Facebook Page</SecondaryHeading>
+            <PrimaryButton
+              isTargetBlank={true}
+              link="https://www.facebook.com/OshadhaSir"
+              type="link"
+            >
+              Follow
+            </PrimaryButton>
+          </CTAItem>
+        </CTABox>
+      </CanvasFG>
     </Canvas>
   );
 };
--- components/layout/homepage/coming-soon/styles.ts
+++ components/layout/homepage/coming-soon/styles.ts
@@ -3,36 +3,118 @@ import styled from "styled-components";
 const Canvas = styled.div`
   grid-column: full-start / full-end;
   width: 100%;
-  height: 100vh;
-  max-height: 100vh;
+  height: auto;
+  position: relative;
+`;
+
+const CanvasBG = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  min-height: 100vh;
+  height: 100%;
+  z-index: -1;
+  overflow: hidden;
+`;
+
+const BackgroundOverlay = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+
+  background: linear-gradient(
+    120deg,
+    rgba(255, 255, 255, 0.5) 0%,
+    rgba(255, 255, 255, 0.2) 100%
+  );
+  backdrop-filter: blur(100px);
+`;
+
+const BlobOne = styled.svg`
+  position: absolute;
+  top: -20%;
+  right: 0;
+  width: 55rem;
+  height: 40rem;
+  fill: #ffddca;
+`;
+
+const BlobTwo = styled.svg`
+  position: absolute;
+  left: -10%;
+  bottom: 20%;
+  width: 60rem;
+  height: 60rem;
+  fill: #ffcbcb;
+`;
+
+const BlobThree = styled.svg`
+  position: absolute;
+  width: 50rem;
+  height: 40rem;
+  right: -10%;
+  bottom: 0;
+  fill: #ffe0a5;
+`;
+
+const CanvasFG = styled.div`
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+
+  padding: 5rem 20rem;
 
   display: grid;
   grid-template-columns: repeat(2, minmax(min-content, 1fr));
-  grid-template-areas: "te im";
-  background-image: ${(props) => props.theme.gradients.background};
-  align-items: end;
+  grid-template-rows: repeat(2, min-content) 1fr;
+  grid-template-areas:
+    "lo lo"
+    "tx im"
+    "ct ct";
+  row-gap: 10rem;
+  column-gap: 8rem;
+  align-items: center;
   justify-items: center;
 
-  ${(props) => props.theme.responsive.next} {
+  ${(props) => props.theme.responsive.small} {
     grid-template-columns: 1fr;
-    grid-template-rows: repeat(2, min-content);
+    grid-template-rows: repeat(4, min-content);
     grid-template-areas:
-      "te"
-      "im";
+      "lo"
+      "tx"
+      "im"
+      "ct";
+  }
+
+  ${(props) => props.theme.responsive.extraSmall} {
+    padding: 5rem;
   }
 `;
 
+const Logo = styled.img`
+  grid-area: lo;
+  width: 20rem;
+  height: 10rem;
+`;
+
 const TextGroup = styled.div`
-  grid-area: te;
+  grid-area: tx;
   align-self: center;
 
   width: 100%;
   height: auto;
-  padding: 5rem;
 
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
   row-gap: 2rem;
 `;
@@ -46,7 +128,7 @@ const ContactGroup = styled.div`
 `;
 
 const SinhalaText = styled.p`
-  font-family: ${(props) => props.theme.fonts.sinhala};
+  font-family: ${(props) => props.theme.fonts.sinhalaPrimary};
   font-size: 2.4rem;
   font-weight: 400;
   letter-spacing: 0;
@@ -54,9 +136,89 @@ const SinhalaText = styled.p`
 `;
 
 const ImageWrapper = styled.div`
-  width: 100%;
-  height: 100%;
   grid-area: im;
+  width: 80%;
+  height: 40rem;
+
+  box-shadow: ${(props) => props.theme.shadows.close};
+  transition: transform 0.2s ease-in-out, box-shadow 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.03);
+    box-shadow: ${(props) => props.theme.shadows.spread};
+  }
+
+  ${(props) => props.theme.responsive.small} {
+    width: 100%;
+    height: 50rem;
+  }
 `;
 
-export { Canvas, TextGroup, ContactGroup, SinhalaText, ImageWrapper };
+const CTABox = styled.div`
+  grid-area: ct;
+  width: 100%;
+  height: 100%;
+
+  background: rgba(255, 255, 255, 0.8);
+
+  backdrop-filter: blur(100px);
+  border-radius: 10px;
+  padding: 8rem;
+
+  display: grid;
+  grid-template-columns: repeat(3, minmax(min-content, 1fr));
+  align-items: start;
+  justify-items: center;
+  gap: 8rem;
+
+  ${(props) => props.theme.responsive.small} {
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(3, 1fr);
+  }
+
+  ${(props) => props.theme.responsive.tiny} {
+    padding: 5rem 3rem;
+  }
+`;
+
+const CTAItem = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(2, min-content) 1fr;
+  align-items: start;
+  justify-items: center;
+  row-gap: 3rem;
+
+  & > *:last-child {
+    align-self: end;
+  }
+`;
+
+const CTAIcon = styled.svg`
+  width: 4rem;
+  height: 4rem;
+
+  & > * {
+    box-shadow: ${(props) => props.theme.shadows.close};
+  }
+`;
+
+export {
+  Canvas,
+  CanvasBG,
+  BackgroundOverlay,
+  BlobOne,
+  BlobTwo,
+  BlobThree,
+  CanvasFG,
+  Logo,
+  TextGroup,
+  ContactGroup,
+  SinhalaText,
+  ImageWrapper,
+  CTABox,
+  CTAItem,
+  CTAIcon,
+};
