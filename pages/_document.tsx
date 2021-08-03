b4b2873d71623aa128642bca0b5271b56db9d412
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2021-08-03 12:54:14 +0530

    amended 3rd Party font import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -50,7 +50,7 @@ class AppDocument extends Document {
             crossOrigin="anonymous"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&family=Source+Sans+Pro:wght@200;300;400;600;700;900display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700;900&family=Playfair+Display:wght@700&display=swap"
             rel="stylesheet"
           />
         </Head>
